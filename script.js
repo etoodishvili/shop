152173117d8595ce04caf394a2429abc49859374
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-07-18 16:59:48 +0400

    add

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let navbarlinks = document.getElementById('navbarlinks');
 togglebutton.addEventListener('click',function() {
   navbarlinks.classList.toggle('active');
 })
+
+
+
+
+const productPreview = document.querySelector('.product_preview');
+const bottomSide = document.querySelector('.bottom-side');
+const images = document.querySelectorAll('img');
+console.log(images)
