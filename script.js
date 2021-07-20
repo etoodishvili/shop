d741fa436f88e36628f2f73db72a659da5cec72e
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-07-20 15:04:09 +0400

    add

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ const productPreview = document.querySelector('.product_preview');
 const bottomSide = document.querySelector('.bottom-side');
 const images = document.querySelectorAll('img');
 console.log(images)
+const topSide = document.createElement('div');
+productPreview.appendChild(topSide);
+topSide.classList.add('top-side');
+topSide.style.cssText+= `background-image: url('images/paraiso1.jpg')`;
+
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        let src = image.src;
+        topSide.style.cssText+= `background-image: url(${src})`;
+        
+    })
+})
