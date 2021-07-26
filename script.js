1bd655fef68183d916abe4fba763080833436d02
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-07-26 04:07:14 +0400

    add

--- script.js
+++ script.js
@@ -24,3 +24,57 @@ images.forEach(image => {
         
     })
 })
+
+
+const sizes = document.querySelectorAll('.size');
+
+function changeSize(){
+  sizes.forEach(size=> size.classList.remove('active'));
+  this.classList.add('active');
+}
+
+sizes.forEach(size => size.addEventListener('click',changeSize));
+
+
+
+
+
+
+
+const bagimage1 = document.querySelector('.bagimage1');
+const trash = document.querySelector('#trash');
+trash.addEventListener('click', ( )=> {
+  bagimage1.classList.remove('.bagimage1');
+
+})
+
+// star
+
+$('.starRating span').click(function(){
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+  $(this).parent().addClass('starRated');
+  
+  // Added for Demo
+  let rating = $(this).index() + 1;
+  $('#currentRating').html( "<small>Rating: <b>" + rating + "</b></small>" );
+});
+
+
+
+// dots
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
